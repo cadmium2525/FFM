67ab8599b5b4cbd7501541604db912aae834632b
cadmium2525 <cadmium2525@gmail.com>
2026-08-15 03:30:11 +0900

    Fix PWA VFX cache revision

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'ff-crystal-rush-pwa-v31';
+const CACHE_NAME = 'ff-crystal-rush-pwa-v32';
 const APP_SHELL = [
   './',
   './index.html',
   './app.js',
   './css/style.css?v=ff5-system-v4',
   './css/ff5-ui.css?v=ff5-system-v4',
-  './css/battle-effects.css?v=ff5-system-v4',
+  './css/battle-effects.css?v=ff5-system-v5',
   './firebase-config.js',
   './manifest.webmanifest',
   './assets/icons/icon-192.png',
